ef6685d49ec899d53109e37d8cc4d0e86c676d3a
Juan Fraire <groteck@gmail.com>
2020-01-03 15:22:12 +0000

    Expose the IBAN.Types

--- src/IBAN/Types.elm
+++ src/IBAN/Types.elm
@@ -7,19 +7,40 @@ module IBAN.Types exposing
     , IBAN(..)
     )
 
+{-| This module have all the types needed to interact with the main IBAN module
 
+@docs IBAN
+@docs CheckCode
+@docs BBAN
+@docs Error
+@docs Format
+@docs Country
+
+-}
+
+
+{-| Main IBAN type, that the information decomposed into different chunks
+of information
+-}
 type IBAN
     = IBAN Country CheckCode BBAN
 
 
+{-| Simple alias for a string that represent the CheckCode
+-}
 type alias CheckCode =
     String
 
 
+{-| Simple alias for a string that represent the BBAN
+-}
 type alias BBAN =
     String
 
 
+{-| This types can be use in case of error to serialize into specific text
+messages on the interface
+-}
 type Error
     = IBANLengthError Country { actual : Int, expected : Int }
     | InvalidCharacter
@@ -27,11 +48,17 @@ type Error
     | UnknownCountryCode String
 
 
+{-| This type is needed to transform and `IBAN
+-}
 type Format
     = Textual
     | Electronic
 
 
+{-| List of countries with `IBAN` type into a string
+
+```
+-}
 type Country
     = Albania
     | Algeria
